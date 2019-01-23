dcc44c4e00654a21b1f1ec6b7b4b8f0e2728078d
ionadmin <ionadmin@fr-w-c135462>
2019-01-23 07:59:22 -0500

    Updated with tmp code to run plugin.

--- QC_Collector.py
+++ QC_Collector.py
@@ -11,18 +11,31 @@ class QC_Collector(IonPlugin):
     Plugin to collect QC metrics from an Ion Torrent run, and generate a CSV
     file that can be loading into tracking databases downstream.
     """
-    version = '0.1.011519'
+    version = '0.2.012219'
     major_block = False
     runtypes = [RunType.FULLCHIP, RunType.THUMB, RunType.COMPOSITE]
     runlevels = [RunLevel.DEFAULT]
 
     def launch(self, data=None):
-        cmd = ['qc_collector_plugin.py', '-v', self.version, '-d', 
-                'startplugin.json', 'barcodes.json']
-        plugin = subprocess.Popen(cmd, stdout=subprocess.PIPE, 
-                stderr=subprocess.PIPE)
-        plugin.communicate()
-        sys.exit(plugin.poll())
+        sys.stderr.write('Writing output to "data.csv"\n')
+
+        with open('data.csv', 'w') as outfh:
+            for var in os.environ:
+                outfh.write('{},{}\n'.format(var, os.environ[var]))
+
+        rundate = os.environ.get('TSP_RUN_DATE', None)
+        runid = os.environ.get('TSP_RUNID', None)
+        sys.stderr.write('run date: %s\n' % rundate)
+        sys.stderr.write("run id: %s\n" % runid)
+
+        sys.exit()
+    #def launch(self, data=None):
+        #cmd = ['qc_collector_plugin.py', '-v', self.version, '-d', 
+                #'startplugin.json', 'barcodes.json']
+        #plugin = subprocess.Popen(cmd, stdout=subprocess.PIPE, 
+                #stderr=subprocess.PIPE)
+        #plugin.communicate()
+        #sys.exit(plugin.poll())
 
 if __name__ == '__main__':
     PluginCLI()
