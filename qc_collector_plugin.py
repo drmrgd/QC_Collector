1ea5d68f970b5f05cc56b8d85cb9168349bccb68
Dave Sims <misc@lamneth.net>
2019-01-31 16:51:23 -0500

    Finished data collection. Just need to work output now.

--- qc_collector_plugin.py
+++ qc_collector_plugin.py
@@ -14,6 +14,7 @@ import inspect
 import argparse
 import subprocess
 import datetime
+import shutil
 
 from collections import defaultdict
 from pprint import pprint as pp
@@ -43,59 +44,63 @@ def get_plugin_config():
 
     plugin_params['version'] = args.version
 
+    # XXX
+    # TODO: revert this.
+    tmp_results_dir = '/Users/simsdj/Dropbox/git_repos/QC_Collector/work_dir/plugin_out/QC_Collector.9999'
+    os.chdir(tmp_results_dir)
+    args.start_plugin_file = os.path.basename(args.start_plugin_file)
+    args.barcodes_file = os.path.basename(args.barcodes_file)
+    ##########################################################################
+
     # Start first with startplugin.json
     start_plugin_json = read_json(args.start_plugin_file)
 
-    plugin_params['plan_name'] = start_plugin_json['plan'].get('planName', None)
     instrument = start_plugin_json['expmeta'].get('instrument', None)
-    plugin_params['instrument'] = plugin_results['chip_data']['instrument'] = instrument
-    plugin_params['results_name'] = start_plugin_json['expmeta'].get(
-        'results_name', None)
-    plugin_params['analysis_dir'] = start_plugin_json['runinfo'].get(
-        'analysis_dir', None)
+    plugin_params['plugin_name']      = start_plugin_json.get('plugin_name', None)
+    plugin_params['plan_name']        = start_plugin_json['plan'].get('planName', None)
+    plugin_params['instrument']       = plugin_results['chip_data']['instrument'] = instrument
+    plugin_params['results_name']     = start_plugin_json['expmeta'].get('results_name', None)
+    plugin_params['output_file_stem'] = start_plugin_json['expmeta'].get('output_file_name_stem', None)
+    plugin_params['analysis_dir']     = start_plugin_json['runinfo'].get('analysis_dir', None)
 
     # TODO: Fix thius path when we deplor
     writelog('warn', '\033[38;5;196m====================>  FIXME!! <===========================\n\t\tSet plugin results to TS env\033[00m\n')
     # plugin_params['plugin_results'] = start_plugin_json['runinfo'].get(
         # 'results_dir', None)
-    plugin_params['plugin_results'] = os.path.abspath(os.path.dirname(__file__))
-    plugin_params['plugin_tmp'] = os.path.join(plugin_params['plugin_results'], 
-        'tmp')
+    plugin_params['plugin_results'] = tmp_results_dir
+
+    plugin_params['plugin_tmp'] = os.path.join(plugin_params['plugin_results'], 'tmp')
+    if os.path.exists(plugin_params['plugin_tmp']):
+        writelog('warn', 'Found previous plugin_tmp dir. Removing to make room '
+            'for new data.')
+        shutil.rmtree(plugin_params['plugin_tmp'])
     os.mkdir(plugin_params['plugin_tmp'])
 
     # TODO: Fix this path when we deploy
     writelog('warn', '\033[38;5;196m====================>  FIXME!! <===========================\n\t\tSet plugin root directory to TS env\033[00m\n')
-    # plugin_params['plugin_root'] = start_plugin_json['runinfo'].get(
-        # 'plugin_dir', None)
-    plugin_params['plugin_root'] = os.path.dirname(os.path.abspath(__file__))
+    # plugin_params['plugin_root'] = start_plugin_json['runinfo'].get('plugin_dir', None)
+    plugin_params['plugin_root'] = '/Users/simsdj/Dropbox/git_repos/QC_Collector'
+    plugin_params['plugin_bin']  = os.path.join(plugin_params['plugin_root'], 'scripts')
+    ###########################################################################################3
 
-    plugin_params['plugin_bin'] = os.path.join(plugin_params['plugin_root'], 
-        'scripts')
-    plugin_params['run_type'] = start_plugin_json['plan']['runType']
-
-    plugin_params['ir_ip'] = start_plugin_json['runinfo']['plugin']['pluginconfig'].get('ip_address', None)
-    plugin_params['ir_token'] = start_plugin_json['runinfo']['plugin']['pluginconfig'].get('api_token', None)
+    plugin_params['run_type']    = start_plugin_json['plan']['runType']
+    plugin_params['ir_ip']       = start_plugin_json['runinfo']['plugin']['pluginconfig'].get('ip_address', None)
+    plugin_params['ir_token']    = start_plugin_json['runinfo']['plugin']['pluginconfig'].get('api_token', None)
 
     # Get some results from some of the data collected so far.
     regex = re.compile('Auto.*?%s-([0-9]{3})-%s_[0-9]+$' %
         (plugin_params['instrument'], plugin_params['plan_name']))
     plugin_results['chip_data']['run_num'] = regex.search(plugin_params['results_name']).group(1)
-
     plugin_results['chip_data']['analysis_date'] = start_plugin_json['expmeta'].get(
         'analysis_date', None)
-
     plugin_results['chip_data'].update(start_plugin_json['pluginconfig'])
 
     # Now read samples from barcodes.json
     bc_json = read_json(args.barcodes_file)
     plugin_results['sample_data'] = get_samples(bc_json)
 
-    '''
-    DEBUG
-    '''
-    writelog('debug', "Data as obtained by startplugin.json and barcodes.json")
-    writelog('debug', 'params dict:\n\t%s\n' % pformat(dict(plugin_params), indent=8))
-    writelog('debug', 'results dict:\n\t%s\n' % pformat(dict(plugin_results), indent=8))
+    writelog('debug', 'Parameters passed to plugin from startplugin.json:\n\t%s\n' 
+        % pformat(dict(plugin_params), indent=8))
 
 def get_samples(bc_json):
     """
@@ -165,16 +170,17 @@ def get_sample_stats():
     # TODO: Need to get paths to the <sample>_rawlib.ionstats_alignment.json files.
     writelog('warn', '\033[38;5;196m====================>  FIXME!! <===========================\n\t\tReset the `file_dir variable\033[00m\n')
     # file_dir = plugin_params['analysis_dir']
-    file_dir = os.path.join(os.path.dirname(__file__), 'work_dir',
-        'resource_files')
+    # file_dir = os.path.join(os.path.dirname(__file__), 'work_dir',
+        # 'resource_files')
+    file_dir = '/Users/simsdj/Dropbox/git_repos/QC_Collector/work_dir/resource_files'
 
     # Proces all samples in the list. Store DNA and RNA separately.
     # for sample in sample_list:
     for bc, sample in plugin_samples.items():
         ionstats_file = file_dir + '/%s_rawlib.ionstats_alignment.json' % bc
         if os.path.exists(ionstats_file):
-            results[sample] = read_ionstats_file(ionstats_file, 'sample')
-            for nt in plugin_results['sample_data'][sample].values():
+            results[sample[0]] = read_ionstats_file(ionstats_file, 'sample')
+            for nt in plugin_results['sample_data'][sample[0]].values():
                 if bc == nt['barcode']:
                     nt.update(read_ionstats_file(ionstats_file, 'sample'))
                     continue
@@ -199,7 +205,6 @@ def get_vcf_metrics():
     the QC metric data we need.  Will require `ir_api_retrieve.py`, 
     `extract_ir_data.sh`, and `get_metrics_from_vcf.py` helper scripts.
     """
-    
     vcfdir, vcfs = get_vcfs(plugin_params['ir_ip'], plugin_params['ir_token'],
         plugin_results['chip_data']['analysis_date'])
     writelog('debug', 'VCFs to process:')
@@ -326,6 +331,74 @@ def get_metrics_from_vcf(vcfs, dna_only):
             results[sample].update({e : fields[i]})
     return results
 
+def get_coverage_analysis_data():
+    """
+    For each sample, read the coverageAnalysis plugin data, and add results to
+    our plugin_results dict.  Determine sample list from global plugin_samples
+    dict.
+    """
+    ca_results_dir = get_latest_plugin_result('coverageAnalysis', 
+            os.path.dirname(plugin_params['plugin_results']))
+
+    # Probably can simplify this by just reading the `results.json` file!
+    for barcode, sample in plugin_samples.items():
+        if sample[1] == 'DNA':
+            # Can only get coverage analysis data on DNA samples, so skip RNA.
+            ca_file = os.path.join(ca_results_dir, barcode, '%s_%s.stats.cov.txt' 
+                % (barcode, plugin_params['output_file_stem']))
+            writelog('debug', 'Reading coverageAnalysis file: %s' % os.path.basename(ca_file))
+            writelog('info', 'Reading coverageAnalysis data for sample: %s.' % sample[0])
+            res = parse_ca_file(ca_file)
+            plugin_results['sample_data'][sample[0]]['DNA'].update(res)
+
+def parse_ca_file(ca_file):
+    """
+    Read and extract pertinent data from the coverageAnalysis plugin ouput
+    data.
+    """
+    wanted_terms = ('Uniformity of base coverage', 'Average base coverage depth',
+        'Amplicons with at least 100 reads', 'Amplicons with at least 500 reads')
+    with open(ca_file) as fh:
+        ca_data = dict(re.split(':\s+', line.rstrip('\n')) for line in fh if ':' in line)
+        return dict(zip(['uniformity', 'mean_depth', '100x_amps', '500x_amps'],
+                [ca_data[x] for x in wanted_terms]))
+
+def get_latest_plugin_result(plugin_name, plugin_dir):
+    """
+    From a plugin name, find the most recent version and return that path.
+    """
+    plugins = {}
+    all_results = os.listdir(plugin_dir)
+    plugins = dict(reversed(d.split('.')) for d in all_results if d.startswith(plugin_name))
+    latest = max(plugins.keys())
+    writelog('debug', 'Latest %s plugin is: %s.%s.' % (plugin_name, plugins[latest],
+        latest))
+    return os.path.join(plugin_dir, '%s.%s' % (plugins[latest], latest))
+
+def get_sampleID():
+    """
+    Read the sampleID output and add the sampleID string to the dataset for downstream 
+    analysis, comparison, and QC tracking.
+    """
+    sid_results_dir = get_latest_plugin_result('sampleID', os.path.dirname(
+        plugin_params['plugin_results']))
+
+    results_json = os.path.join(sid_results_dir, 'results.json')
+    with open(results_json) as fh:
+        jdata = json.load(fh)
+        for barcode, sample in plugin_samples.items():
+            if sample[1] == 'DNA':
+                sid = jdata['barcodes'][barcode].get('Sample ID', None)
+                plugin_results['sample_data'][sample[0]]['DNA']['sample_id'] = sid
+def cleanup():
+    """
+    Get rid of any temp files and extraneous data we don't want laying around. 
+    For now keep it simple and just get rid of anything. Later we can set
+    levels to help determine what level of data we want to keep for debugging
+    purposes.
+    """
+    shutil.rmtree(plugin_params['plugin_tmp'])
+
 def read_json(jfile):
     with open(jfile) as fh:
         return json.load(fh)
@@ -362,19 +435,20 @@ def __exit__(msg=None):
     sys.exit()
 
 def plugin_main():
-    writelog('info', 'QC Collector version has started.')
-
     get_plugin_config()
 
     # Keep track of barcode : sampleid for mapping throughout.
     global plugin_samples
 
+    writelog('info', '***************  %s (version %s) has started. ***********'
+        '****' % (plugin_params['plugin_name'], plugin_params['version']))
     writelog('info', 'Run configuration: ')
     writelog(None, 'Plugin version: {}'.format(plugin_params['version']))
+    writelog(None, 'Plugin results dir: {}'.format(plugin_params['plugin_results']))
     writelog(None, 'Plugin root directory: {}'.format(
         plugin_params['plugin_root']))
     writelog(None, 'Run name: {}'.format(plugin_params['results_name']))
-    writelog(None, 'Results dir: {}'.format(plugin_params['plugin_results']))
+    writelog(None, 'Results dir: {}'.format(plugin_params['analysis_dir']))
     writelog(None, 'Log level is {}'.format(loglevel))
 
     writelog(None, 'Run type is: {}'.format(
@@ -386,7 +460,7 @@ def plugin_main():
         for nt in ('DNA', 'RNA'):
             try:
                 bc = plugin_results['sample_data'][samp][nt]['barcode']
-                plugin_samples[bc] = samp
+                plugin_samples[bc] = (samp, nt)
             except KeyError:
                 # we don't have that nucleic acid type.
                 continue
@@ -397,15 +471,13 @@ def plugin_main():
     writelog('warn', '\033[38;5;196m====================>  FIXME!! <===========================\n\t\tFix paths for serialized and basecall JSON files.\033[00m\n')
     '''
     TODO: Fix this for plugin
-    basecaller_json = os.path.join(plugin_params['analysis_dir'], 
-        'basecaller_results', 'BaseCaller.json')
     serialized_json = os.path.join(plugin_params['analysis_dir'], 
         'serialized_%s.json' % plugin_params['results_name'])
+    basecaller_json = os.path.join(plugin_params['analysis_dir'], 
+        'basecaller_results', 'BaseCaller.json')
     '''
-    basecaller_json = os.path.join(os.path.dirname(__file__), 'work_dir',
-        'resource_files','BaseCaller.json')
-    serialized_json = os.path.join(os.path.dirname(__file__), 'work_dir',
-        'resource_files', 'serialized_%s.json' % plugin_params['results_name'])
+    serialized_json = '/Users/simsdj/Dropbox/git_repos/QC_Collector/work_dir/resource_files/serialized_Auto_user_S5-MC4-299-20190118.OCAv3.Sequencing.OT2.MSM_441.json'
+    basecaller_json = '/Users/simsdj/Dropbox/git_repos/QC_Collector/work_dir/resource_files/BaseCaller.json'
 
     # Finish getting the chip level metrics.
     get_chip_metrics(basecaller_json, serialized_json)
@@ -415,11 +487,29 @@ def plugin_main():
     get_sample_stats()
 
     # Now collect the VCFs and get those data. 
+    #writelog('warn', '\033[38;5;196m====================>  FIXME!! <===========================\n\t\tneed to add function back into pipeline after testing.\033[00m\n')
     get_vcf_metrics()
 
-    pp(dict(plugin_results))
-    __exit__("Stopping after collecting most of metrics.  Now we need to map the"
-        " coverageAnalysis plugin data to finish up.")
+    # Retrieve the coverageAnalysis plugin results and add them to the plugin_results.
+    get_coverage_analysis_data()
+
+    # Get the sampleID string and add that in.
+    get_sampleID()
+
+    # Not sure if framework writes a results.json file, but it's definitely 
+    # handy, so let's make one!
+    writelog('debug', 'Writing data to results.json for safekeeping.')
+    outjson = os.path.join(plugin_params['plugin_results'], 'results.json')
+    with open(outjson, 'w') as outfh:
+        json.dump(plugin_results, outfh, indent=4, sort_keys=True)
+
+    # DEBUG: TODO: XXX
+    #pp(dict(plugin_results))
+    __exit__("Stopping after collecting the rest of the metrics. Just need to "
+        "work out the print and webpage display method.")
+
+    writelog('info', 'Finished generating data. Running cleanup.')
+    cleanup()
 
     # TODO: Need to clean up temp files from `get_vcf_metrics()` and 
             # get_coverage_analysis_data(). Store the temp directory path in 
